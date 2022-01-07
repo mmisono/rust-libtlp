d708d3e7cd2937118f40bd8d5d32c42a90fc6496
Masanori Misono <m.misono760@gmail.com>
2022-01-07 15:37:14 +0900

    Use clap for argument parser

--- examples/dma_read.rs
+++ examples/dma_read.rs
@@ -5,60 +5,58 @@ use libtlp::{pci, DmaDirection, NetTlp};
 use std::net::Ipv4Addr;
 
 use anyhow::Result;
-use structopt::StructOpt;
+use clap::Parser;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "dma_read")]
-struct Opt {
+#[derive(Parser, Debug)]
+#[clap(about, version)]
+struct Args {
     /// Bus:Device.Function of NetTLP Adapter, "xx:xx.x"
-    #[structopt(short = "b", long = "bdf")]
+    #[clap(short, long)]
     bdf: pci::Bdf,
 
     /// Local address at NetTLP link
-    #[structopt(short = "l", long = "local")]
+    #[clap(short, long = "local")]
     local_addr: Ipv4Addr,
 
     /// Remote address at NetTLP link
-    #[structopt(short = "r", long = "remote")]
+    #[clap(short, long = "remote")]
     remote_addr: Ipv4Addr,
 
     /// TLP tag
-    #[structopt(short = "t", long = "tag", default_value = "0")]
+    #[clap(short, long, default_value_t = 0)]
     tag: u8,
 
     /// Target address
-    #[structopt(
-        short = "a",
-        long = "addr",
-        default_value = "0",
+    #[clap(
+        short, long, default_value_t = 0,
         parse(try_from_str = parse_int::parse)
     )]
     addr: u64,
 
     /// Transfer size (bytes)
-    #[structopt(short = "s", long = "size", default_value = "4")]
+    #[clap(short, long, default_value_t = 4)]
     size: usize,
 
     /// MaxReadRequestSize
-    #[structopt(short = "m", long = "mrrs", default_value = "512")]
+    #[clap(short, long, default_value_t = 512)]
     mrrs: usize,
 }
 
 fn main() -> Result<()> {
-    let opt = Opt::from_args();
+    let args = Args::parse();
     let dir = DmaDirection::DmaIssuedByLibTLP;
     let nettlp = NetTlp::new(
-        opt.bdf,
-        opt.local_addr,
-        opt.remote_addr,
-        opt.tag,
-        opt.mrrs,
+        args.bdf,
+        args.local_addr,
+        args.remote_addr,
+        args.tag,
+        args.mrrs,
         dir,
     )?;
     dbg!(&nettlp);
 
-    let mut buf = vec![0; opt.size];
-    nettlp.dma_read(opt.addr, &mut buf)?;
+    let mut buf = vec![0; args.size];
+    nettlp.dma_read(args.addr, &mut buf)?;
     dbg!(&buf);
 
     Ok(())
